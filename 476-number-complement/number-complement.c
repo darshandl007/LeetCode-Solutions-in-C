7ee28c8e06b121332abb89c747a55853da27cbad
Darshan D L <darshandldl878@gmail.com>
2026-08-17 19:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (63.25%) - LeetSync

--- 476-number-complement/number-complement.c
+++ 476-number-complement/number-complement.c
@@ -1,14 +1,9 @@
 int findComplement(int num) {
-
-    int bit=0;
-    int temp=num;
     int mask=0;
-
-   while (temp)
-    {
-        mask = (mask << 1) | 1;
-        temp = temp >> 1;
+    int temp=num;
+    while(temp){
+        mask=mask<<1 | 1;
+        temp=temp>>1;
     }
-
     return (~num) & mask;
 }
