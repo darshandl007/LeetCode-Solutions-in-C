4d9443f5e6e4a18e3192c6229a0ac57f766a2d79
Darshan D L <darshandldl878@gmail.com>
2026-08-27 20:03:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (59.08%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.c
@@ -0,0 +1,16 @@
+int rangeBitwiseAnd(int left, int right) {  
+ if(left==0 && right==0)
+ {
+    return 0;
+ }
+ int count =0;
+while(left != right)
+{
+    left=left>>1;
+    right=right>>1;
+    count++;
+}
+left=left<<count;
+
+return left;
+}
