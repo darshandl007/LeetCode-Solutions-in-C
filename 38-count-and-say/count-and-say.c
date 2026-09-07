19f34eb7479f926f785de81beae282bae1e46292
Darshan D L <darshandldl878@gmail.com>
2026-09-07 19:31:03 +0530

    Time: 7 ms (26.36%) | Memory: 9.4 MB (53.19%) - LeetSync

--- 38-count-and-say/count-and-say.c
+++ 38-count-and-say/count-and-say.c
@@ -1,30 +1,30 @@
 char* countAndSay(int n) {
-    
-    char *str= malloc(50000*sizeof(char));
-    char *pre=malloc(50000);
-    
-   strcpy(str,"1");
+
+    char *current=malloc(5000);
+    char *prev=malloc(5000);
+
+    strcpy(current,"1");
+
     for(int i=1;i<n;i++)
     {
         int j=0;
         int pos=0;
-            while(str[j] != '\0')
-            {
-                    char digit = str[j];
-                    int count =0;
-                    while(str[j] == digit)
-                    {
-                        count++;
-                        j++;
-                    }
-                  pos= pos +  sprintf(pre+pos,"%d%c",count,digit);
-            }
-            pre[pos]='\0';
-            strcpy(str,pre);
+        while(current[j] != '\0')
+        {
+                char digit=current[j];
+                int count=0;
+                while(current[j] == digit)
+                {
+                    j++;
+                    count++;
+                }
+                pos=pos+sprintf(pos+prev,"%d%c",count,digit);
+        }
+        prev[pos]='\0';
+        strcpy(current,prev);
     }
 
+    return current;
 
 
-   free(pre);
-    return str;
 }
